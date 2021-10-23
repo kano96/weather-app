aa8db06b22e363b1e41dea14868ceb417f902ead
Kevin Torres <kevin_akd@hotmail.com>
2021-10-22 22:48:14 -0500

    añadidos estilos a la pagina about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,20 +3,22 @@ import styles from './about.module.css'
 
 function About() {
     return (
-        <div className={styles.aboutcontainer}>
-            <div className={styles.title}>
-                <h3>About Weather Application</h3>
-            </div>
-            <div className={styles.desc}>
-                <p>
-                    This application is created with React.js, 
-                    it takes the data from OpenWeather API and
-                    show it when you add a city from the search
-                    bar in the navigation bar. This project is 
-                    part of the 'Soy Henry' bootcamp. Hope you 
-                    enjoy it!
-                </p>
-                <span>By: Kevin Torres</span>
+        <div className={styles.aboutsection}>
+            <div className={styles.aboutcontainer}>
+                <div className={styles.title}>
+                    <h3>About Weather Application</h3>
+                </div>
+                <div className={styles.desc}>
+                    <p>
+                        This application was created with React.js, 
+                        it takes the data from OpenWeather API and
+                        show it when you add a city from the search
+                        bar in the navigation bar. This project is 
+                        part of the 'Soy Henry' bootcamp. Hope you 
+                        enjoy it!
+                    </p>
+                    <span>By: Kevin Torres</span>
+                </div>
             </div>
         </div>
     )
